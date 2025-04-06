c898b179150a9c0427bf8eb9799201c92cf3d49b
Veldor09 <arayavegad@gmail.com>
2025-04-05 20:34:01 -0600

    propsprueba1

---
+++ lab3y4/src/Components/AnonymousUser.jsx
@@ -0,0 +1,8 @@
+export default function AnonymousUser() {
+
+    return (
+        <>
+            <h2>Hola, por favor accede al sition para obtener mayor informacion</h2>
+        </>
+    )
+}
---
+++ lab3y4/src/Components/WelcomeWidget.jsx
@@ -0,0 +1,8 @@
+export default function WelcomeWidget({email}) {
+    
+    return (
+        <>
+            <h2>Welcome to our site {email}</h2>
+        </>
+    )
+}
